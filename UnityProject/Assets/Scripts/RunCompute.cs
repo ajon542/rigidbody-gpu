444dc88680d9aa278820d791e942709e8539341f
Andrew Jones <ajon542@gmail.com>
2016-05-10 15:00:30 -0700

    Setup simple scene to run a compute shader example

--- UnityProject/Assets/Scripts/RunCompute.cs
+++ UnityProject/Assets/Scripts/RunCompute.cs
@@ -1,15 +1,58 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class RunCompute : MonoBehaviour {
+/// <summary>
+/// Example of running a simple compute shader to fill a buffer.
+/// </summary>
+public class RunCompute : MonoBehaviour
+{
+    public ComputeShader shader;
+    private ComputeBuffer buffer;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private int kernelHandle;
+    private int groupCount = 2;
+    private int threadCount = 4;
+
+    private int bufferSize;
+    private int[] data;
+
+    private void Start()
+    {
+        // Calculate the buffer size.
+        bufferSize = groupCount * threadCount;
+        data = new int[bufferSize];
+
+        // Create compute buffer.
+        buffer = new ComputeBuffer(bufferSize, sizeof(int));
+
+        // Obtain the handle to the kernel to run.
+        kernelHandle = shader.FindKernel("CSMain");
+
+        // Set the buffer on the compute shader.
+        shader.SetBuffer(kernelHandle, "buffer", buffer);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            // Fill the buffer using the compute shader.
+            shader.Dispatch(kernelHandle, groupCount, 1, 1);
+
+            // Obtain the data.
+            // NOTE: This is inefficient as it is essentially copying the data from the GPU.
+            buffer.GetData(data);
+
+            // Display the data.
+            for (int i = 0; i < bufferSize; i++)
+            {
+                Debug.Log(data[i]);
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // Release the buffer.
+        buffer.Release();
+    }
 }
