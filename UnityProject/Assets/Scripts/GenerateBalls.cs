0999b9f9eb545121881227a041204425f84e9e0f
Andrew Jones <ajon542@gmail.com>
2016-05-15 13:37:23 -0700

    Generate random ball prefabs

--- UnityProject/Assets/Scripts/GenerateBalls.cs
+++ UnityProject/Assets/Scripts/GenerateBalls.cs
@@ -1,25 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 
 public class GenerateBalls : MonoBehaviour
 {
-    public int ballCount = 10;
-    public GameObject ball;
-
+    public int ballCount = 5000;
     private List<GameObject> balls = new List<GameObject>();
-
 
     void Start()
     {
         GameObject go;
         for (int i = 0; i < ballCount; ++i)
         {
+            // Random position.
             Vector3 randomPosition = new Vector3(Random.value * 20, Random.value * 20, Random.value * 20);
-            //go = (GameObject)Instantiate(ball, randomPosition, Quaternion.identity);
 
-            go = (GameObject)Instantiate(Resources.Load("Ball15", typeof(GameObject)), randomPosition, Quaternion.identity);
+            // Random ball prefab.
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Ball");
+            sb.Append(Random.Range(1, 16));
 
+            // Instantiate the prefab.
+            go = (GameObject)Instantiate(Resources.Load(sb.ToString(), typeof(GameObject)), randomPosition, Quaternion.identity);
+
+            // Set the parent so the hierarchy window in Unity editor is clean.
             go.transform.parent = gameObject.transform;
             balls.Add(go);
         }
@@ -27,6 +32,15 @@ public class GenerateBalls : MonoBehaviour
 
     void Update()
     {
-
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            print("All " + Resources.FindObjectsOfTypeAll<UnityEngine.Object>().Length);
+            print("Textures " + Resources.FindObjectsOfTypeAll<Texture>().Length);
+            print("AudioClips " + Resources.FindObjectsOfTypeAll<AudioClip>().Length);
+            print("Meshes " + Resources.FindObjectsOfTypeAll<Mesh>().Length);
+            print("Materials " + Resources.FindObjectsOfTypeAll<Material>().Length);
+            print("GameObjects " + Resources.FindObjectsOfTypeAll<GameObject>().Length);
+            print("Components " + Resources.FindObjectsOfTypeAll<Component>().Length);
+        }
     }
 }
