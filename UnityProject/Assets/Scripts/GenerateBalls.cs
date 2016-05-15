3805405f2ae65b3e008aa507e2db957c3398a366
Andrew Jones <ajon542@gmail.com>
2016-05-15 12:53:38 -0700

    Update physics scene to generate 5000 balls

---
+++ UnityProject/Assets/Scripts/GenerateBalls.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GenerateBalls : MonoBehaviour
+{
+    public int ballCount = 10;
+    public GameObject ball;
+
+    private List<GameObject> balls = new List<GameObject>();
+
+
+    void Start()
+    {
+        GameObject go;
+        for (int i = 0; i < ballCount; ++i)
+        {
+            Vector3 randomPosition = new Vector3(Random.value * 20, Random.value * 20, Random.value * 20);
+            go = (GameObject)Instantiate(ball, randomPosition, Quaternion.identity);
+            go.transform.parent = gameObject.transform;
+            balls.Add(go);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
