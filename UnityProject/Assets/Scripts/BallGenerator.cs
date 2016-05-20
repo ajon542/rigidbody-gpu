21ae993d8bfb51864961434c1261106e0ed401a5
Andrew Jones <ajon542@gmail.com>
2016-05-19 17:21:21 -0700

    Added custom balls with no rigidbody or collider

--- UnityProject/Assets/Scripts/BallGenerator.cs
+++ UnityProject/Assets/Scripts/BallGenerator.cs
@@ -3,13 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
-public class BallGenerator : MonoBehaviour
+public class BallGenerator
 {
-    public int ballCount = 5000;
-    private List<GameObject> balls = new List<GameObject>();
-
-    void Start()
+    /// <summary>
+    /// Generates a list of ball game objects.
+    /// </summary>
+    /// <remarks>
+    /// It is up to the called to destroy the game objects.
+    /// </remarks>
+    /// <param name="parent">The parent game object for each ball.</param>
+    /// <param name="ballCount">THe number of balls to generate.</param>
+    /// <returns>THe list of generated ball objects.</returns>
+    public List<GameObject> Generate(GameObject parent, int ballCount)
     {
+        List<GameObject> balls = new List<GameObject>();
         GameObject go;
         for (int i = 0; i < ballCount; ++i)
         {
@@ -22,25 +29,12 @@ public class BallGenerator : MonoBehaviour
             sb.Append(Random.Range(1, 16));
 
             // Instantiate the prefab.
-            go = (GameObject)Instantiate(Resources.Load(sb.ToString(), typeof(GameObject)), randomPosition, Quaternion.identity);
+            go = (GameObject)GameObject.Instantiate(Resources.Load(sb.ToString(), typeof(GameObject)), randomPosition, Quaternion.identity);
 
             // Set the parent so the hierarchy window in Unity editor is clean.
-            go.transform.parent = gameObject.transform;
+            go.transform.parent = parent.transform;
             balls.Add(go);
         }
-    }
-
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.P))
-        {
-            print("All " + Resources.FindObjectsOfTypeAll<UnityEngine.Object>().Length);
-            print("Textures " + Resources.FindObjectsOfTypeAll<Texture>().Length);
-            print("AudioClips " + Resources.FindObjectsOfTypeAll<AudioClip>().Length);
-            print("Meshes " + Resources.FindObjectsOfTypeAll<Mesh>().Length);
-            print("Materials " + Resources.FindObjectsOfTypeAll<Material>().Length);
-            print("GameObjects " + Resources.FindObjectsOfTypeAll<GameObject>().Length);
-            print("Components " + Resources.FindObjectsOfTypeAll<Component>().Length);
-        }
+        return balls;
     }
 }
---
+++ UnityProject/Assets/Scripts/UnityStandardPhysicsExample.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UnityStandardPhysicsExample : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        BallGenerator bg = new BallGenerator();
+        List<GameObject> balls = bg.Generate(gameObject, 1000);
+    }
+}
